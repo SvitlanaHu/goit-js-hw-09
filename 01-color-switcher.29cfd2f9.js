455ee989e9ac73088bd64350f4c454970cde8919
SvitlanaHu <138813162+SvitlanaHu@users.noreply.github.com>
2023-11-24 11:04:19 +0000

    Deploying to gh-pages from @ SvitlanaHu/goit-js-hw-09@de38e06f21d9396e06f23c30b50c68769316cad9 🚀

---
+++ 01-color-switcher.29cfd2f9.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),r=document.querySelector("[data-stop]");let a=null;r.setAttribute("disabled",""),e.addEventListener("click",(e=>{e.target.setAttribute("disabled",!0),r.removeAttribute("disabled"),a=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),r.addEventListener("click",(t=>{t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.29cfd2f9.js.map
